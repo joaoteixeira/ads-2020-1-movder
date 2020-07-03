66b9b9ed9ab5443920151c4812f688338385787c
Elyton Diniz <elytondiniz@hotmail.com>
2020-07-03 14:35:26 -0400

    melhorias nas migrantions

--- database/migrations/2020_04_24_184310_relaciona_movimentos_vistorias.php
+++ database/migrations/2020_04_24_184310_relaciona_movimentos_vistorias.php
@@ -13,7 +13,7 @@ class RelacionaMovimentosVistorias extends Migration
      */
     public function up()
     {
-        Schema::create('movimento_vistoria', function (Blueprint $table) {
+        Schema::create('movimentos_vistorias', function (Blueprint $table) {
             $table->integer('movimento_id')->unsigned()->default(1);
             $table->foreign('movimento_id')->references('id')->on('movimentos');
             $table->integer('vistoria_id')->unsigned()->default(1);
